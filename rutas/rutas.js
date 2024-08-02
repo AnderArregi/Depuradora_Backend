7561ed8aa14dfc36d3fab0caec117d7ace189f05
AnderJabato <115215856+AnderJabato@users.noreply.github.com>
2024-08-02 21:34:46 +0200

    Update rutas.js

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -6,7 +6,11 @@ const { obtenerLogin, obtenerUsuario  } = require('../controladores/controladorU
 const { nuevoPass } = require('../controladores/controladorPass');
 const { depuradora } = require('../controladores/controladorDepuradora');
 const { obtenerTurnos, obtenerTurnosDia, obtenerTurnosDiaUsuario } = require('../controladores/controladorTurno');
-const { insertarFormulario } = require('../controladores/controladorFormulario');
+
+const { insertarFormulario, actualizarFormulario, obtenerFormulario } = require('../controladores/controladorFormulario');
+const { insertarVacacion, actualizarVacacion, obtenerVacacionesMes, borrarVacacion} = require('../controladores/controladorVacacion');
+
+
 const { obtenerPowerBI } = require('../controladores/controladorPowerBI.js');
 
 
@@ -17,7 +21,21 @@ router.get('/usuario/:usuarioId', obtenerUsuario);
 router.get('/turno/:ano/:mes', obtenerTurnos);//Pongo año y mes para que no sea igual que la BBDD y tenga mas seguridad
 router.get('/turno/:ano/:mes/:dia', obtenerTurnosDia);
 router.get('/turno/:ano/:mes/:dia/:usuarioId', obtenerTurnosDiaUsuario);
+
+
 router.post('/formulario', insertarFormulario);
+router.put('/formulario/:usuarioId/:ano/:mes/:dia/:turno', actualizarFormulario);
+router.get('/formulario/:fecha', obtenerFormulario);
+//router.delete('/formulario/:usuarioId/:ano/:mes/:dia/:turno', borrarFormulario);
+
+
+router.post('/vacacion', insertarVacacion);
+router.put('/vacacion/:IdVacacion', actualizarVacacion);
+router.get('/vacacion/:anio/:mes', obtenerVacacionesMes);
+router.delete('/vacacion/:IdVacacion', borrarVacacion);
+
+
+
 
 router.get('/powerbi-token', obtenerPowerBI);
 
