503e3d5ccd8eef535a502688c02476aa772ed717
AnderArregi <115215856+AnderArregi@users.noreply.github.com>
2024-11-18 18:38:30 +0100

    Update controladorUsuario.js

--- controladores/controladorUsuario.js
+++ controladores/controladorUsuario.js
@@ -1,25 +1,51 @@
 const connection = require('../config/bbdd')
+const { comprobarToken, quitarToken, obtenerToken } = require('./autentificacion');
 
 const obtenerLogin = (req, res) => {
+
     const { correo, pass } = req.body;
     if (!correo || !pass) {
         return res.status(400).send({ message: 'El Correo y la contraseña son necesarias' });
     }
 
-    const sql = 'SELECT * FROM Usuario WHERE correo = ? AND pass = ?';
+    const sql = 'SELECT id, nombre  FROM Usuario WHERE correo = ? AND pass = ?';//
     connection.query(sql, [correo, pass], (error, results) => {
         if (error) {
             return res.status(500).send({ message: 'Error al consultar la base de datos', error });
         }
 
         if (results.length > 0) {
-            res.send({ message: 'Login exitoso', user: results[0] });
+            const token = obtenerToken(correo, results[0].id);
+            res.send({token,  user: results[0] });
         } else {
             res.status(401).send({ message: 'Usuario o contraseña incorrectos' });
         }
     });
 };
-const obtenerUsuario = (req, res) =>{
+const obtenerLogout = (req, res) => {
+    
+    const  token  = req.headers['x-access-token'];
+    if (!token) {
+        return res.status(403).send({ message: 'El token es obligatorio' });
+    }
+
+   if(comprobarToken(token)){
+    quitarToken(token);
+    return res.status(200).send({ message: 'El logout ha sido exitoso' });
+   }
+   return res.status(401).send({ message: 'El token no es valido'});
+};
+
+const obtenerUsuario = async (req, res) =>{
+    const token = req.headers['x-access-token'];
+    if (!token) {
+        return res.status(401).json({ auth: false, message: 'No token provided.' });
+    }
+    const tokenValido = await comprobarToken(token);
+    if(!tokenValido){
+        return res.status(401).send({ auth: false, message: 'Fallo al autentificar el Token' });     
+    }
+
     const usuarioId = req.params.usuarioId;
     const sql = `SELECT nombre FROM usuario WHERE id = '${usuarioId}'`//comillas en la variable usuario porque es varchar(1) y se necesitan poruq es string
     connection.query(sql, [], (error, results) => {
@@ -34,5 +60,6 @@ const obtenerUsuario = (req, res) =>{
 
 module.exports = {
     obtenerLogin,
+    obtenerLogout,
     obtenerUsuario
 }
