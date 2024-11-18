949e3000094996fff29f79bfb20f8aa9d6eadd5e
AnderArregi <115215856+AnderArregi@users.noreply.github.com>
2024-11-18 19:33:33 +0100

    Update rutas.js

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -2,22 +2,26 @@ const express = require('express')
 const router = express.Router();
 
 
-const { obtenerLogin, obtenerUsuario  } = require('../controladores/controladorUsuario');
+const { obtenerLogin, obtenerLogout, obtenerUsuario  } = require('../controladores/controladorUsuario');
 const { nuevoPass } = require('../controladores/controladorPass');
 const { depuradora } = require('../controladores/controladorDepuradora');
 const { obtenerTurnos, obtenerTurnosDia, obtenerTurnosDiaUsuario } = require('../controladores/controladorTurno');
 
-const { insertarFormulario, actualizarFormulario, obtenerFormulario } = require('../controladores/controladorFormulario');
-const { insertarVacacion, actualizarVacacion, obtenerVacacionesMes, borrarVacacion} = require('../controladores/controladorVacacion');
+const { insertarFormulario, actualizarFormulario, obtenerFormulario, borrarFormulario } = require('../controladores/controladorFormulario');
+const { insertarVacacion, actualizarVacacion, obtenerVacacion, obtenerVacacionesMes, borrarVacacion} = require('../controladores/controladorVacacion');
+const { obtenerDatosM3Operario, obtenerDatosPlanta} = require('../controladores/controladorDatos');
 
 
-const { obtenerPowerBI } = require('../controladores/controladorPowerBI.js');
+//const { obtenerPowerBI } = require('../controladores/controladorPowerBI.js');
 
 
 router.post('/login', obtenerLogin);
+router.post('/logout', obtenerLogout);
+
 router.post('/nuevoPass', nuevoPass);
 router.get('/depuradora/actual', depuradora);
 router.get('/usuario/:usuarioId', obtenerUsuario);
+
 router.get('/turno/:ano/:mes', obtenerTurnos);//Pongo año y mes para que no sea igual que la BBDD y tenga mas seguridad
 router.get('/turno/:ano/:mes/:dia', obtenerTurnosDia);
 router.get('/turno/:ano/:mes/:dia/:usuarioId', obtenerTurnosDiaUsuario);
@@ -25,19 +29,23 @@ router.get('/turno/:ano/:mes/:dia/:usuarioId', obtenerTurnosDiaUsuario);
 
 router.post('/formulario', insertarFormulario);
 router.put('/formulario/:usuarioId/:ano/:mes/:dia/:turno', actualizarFormulario);
-router.get('/formulario/:fecha', obtenerFormulario);
-//router.delete('/formulario/:usuarioId/:ano/:mes/:dia/:turno', borrarFormulario);
+router.get('/formulario/:fecha/:turno', obtenerFormulario);
+router.delete('/formulario/:diaTurno', borrarFormulario);
 
 
 router.post('/vacacion', insertarVacacion);
 router.put('/vacacion/:IdVacacion', actualizarVacacion);
+router.get('/vacacion/:fecha', obtenerVacacion);
 router.get('/vacacion/:anio/:mes', obtenerVacacionesMes);
-router.delete('/vacacion/:IdVacacion', borrarVacacion);
+router.delete('/vacacion/:fecha', borrarVacacion);
+
+
+router.get('/datos', obtenerDatosM3Operario)
+router.get('/datosPlanta', obtenerDatosPlanta)
 
 
 
-
-router.get('/powerbi-token', obtenerPowerBI);
+//router.get('/powerbi-token', obtenerPowerBI);
 
 
 module.exports = router
